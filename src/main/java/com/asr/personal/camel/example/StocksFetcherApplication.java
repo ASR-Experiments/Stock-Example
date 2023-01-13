2d6f8cdfa8576f3ed5e708e0840d7d28b1ac75d5
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2023-01-14 03:07:54 +0530

    Boilerplate addition

---
+++ src/main/java/com/asr/personal/camel/example/StocksFetcherApplication.java
@@ -0,0 +1,13 @@
+package com.asr.personal.camel.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksFetcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocksFetcherApplication.class, args);
+	}
+
+}

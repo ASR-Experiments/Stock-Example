ec360c8ac5d13f802ec5c231006faecd9f3f2570
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2023-01-14 16:57:08 +0530

    Replaced bean autowiring to Spring's IOC

--- src/main/java/com/asr/personal/camel/example/routes/BasicRoute.java
+++ src/main/java/com/asr/personal/camel/example/routes/BasicRoute.java
@@ -2,15 +2,19 @@ package com.asr.personal.camel.example.routes;
 
 import com.asr.personal.camel.example.constants.RouteConstant;
 import com.asr.personal.camel.example.services.MessageGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BasicRoute extends org.apache.camel.builder.RouteBuilder {
 
+  @Autowired
+  MessageGenerator messageGenerator;
+
   @Override
   public void configure() {
     from(RouteConstant.FIRST_ROUTE)
-        .bean(MessageGenerator.class)
+        .bean(messageGenerator)
         .routeId(RouteConstant.FIRST_ROUTE)
         .tracing()
         .to(RouteConstant.FIRST_DIRECT);
